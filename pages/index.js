98b8a33040149fa0dca5c0378a43d5c1cef718e2
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-22 17:25:06 -0700

    adding link to products page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
 import Link from 'next/link';
-import { Button } from 'antd'; 
-ReactDOM.render(<Button />, mountNode);
 
 export default () => <div>Hey there! Welcome to next.js! <Link href="/product">Product</Link></div>
