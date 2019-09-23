9da1959a40a57eaba14a1a06e01b1a739154ecda
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-22 23:54:46 -0700

    putting together the homepage

--- pages/index.js
+++ pages/index.js
@@ -2,3 +2,5 @@ import React from 'react';
 import Link from 'next/link';
 import { Button } from 'antd';
 import { css } from '@emotion/core';
+import { Content, Footer, Header, Container } from '../Components/Layout';
+
