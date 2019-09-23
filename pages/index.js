f0ad3feab42feb3b5e61a3f726a6642c0f343683
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-22 23:58:29 -0700

    working on homepage

--- pages/index.js
+++ pages/index.js
@@ -4,3 +4,10 @@ import { Button } from 'antd';
 import { css } from '@emotion/core';
 import { Content, Footer, Header, Container } from '../Components/Layout';
 
+const mainCls = css`
+    max-width: 1000px;
+    display: flex;
+    flex-direction: row;
+    margin: 0 auto;
+    margin-top: 50px;
+`
